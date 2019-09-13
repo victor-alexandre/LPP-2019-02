eacb3bba51447432f624245dab3b1e010a6f1619
Victor Alexandre <38512776+victor-alexandre@users.noreply.github.com>
2019-09-13 15:39:42 -0300

    Add files via upload

---
+++ Aula-13-09/1ex.lua
@@ -0,0 +1,7 @@
+n = io.read();
+
+for i = 2, n, 2
+do
+	print(i .. "^" .. i .. " = " .. i^2);
+	
+end
---
+++ Aula-13-09/1exVProf.lua
@@ -0,0 +1,6 @@
+local n = io.read("*n");
+
+for i = 2, n, 2 do
+	print(string.format("%d^2 =  %d", i, i^2));
+	
+end
