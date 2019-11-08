f2a999993732d42dd54c10ea4f6d42a89dd39a94
Victor Alexandre <38512776+victor-alexandre@users.noreply.github.com>
2019-11-08 12:44:19 -0300

    Identifica nome e valor de variável
    Adicionado a identificação do nome das variáveis, tamanho do vetor

--- interpretadorVictor.lua
+++ interpretadorVictor.lua
@@ -1,12 +1,13 @@
 -------------------------------Variaveis globais--------------------------------------------------------------------------------
-linhas = {}
+progLines = {}
 functionsTable = {}
+stackExecution = {}
 -------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------
 
 
 -------------------------------Declaração das funções--------------------------------------------------------------------------
---Abre o arquivo e salva ele localmente na variavel global linhas
+--Abre o arquivo e salva ele localmente na variavel global progLines
 function prepareFile()
 	-- Pega o nome do arquivo passado como parâmetro (se houver)
 	local filename = "teste1.bpl"
@@ -20,24 +21,24 @@ function prepareFile()
 	   print(string.format("[ERRO] Cannot open file %q", filename))
 	   os.exit(1)
 	end
-	--Salva o arquivo na variavel global linhas
+	--Salva o arquivo na variavel global progLines
 	saveFile(file:lines())
 
 	file:close()
 end	
 
---Salva o arquivo na variavel global linhas. Observe que ele será salvo com os comentários removidos
+--Salva o arquivo na variavel global progLines. Observe que ele será salvo com os comentários removidos
 function saveFile(file)
 	for line in file do
-		linhas[#linhas + 1] = removeComments(line)
+		progLines[#progLines + 1] = removeComments(line)
 	end
 end
 
 --Procura pelas funções existentes no programa e salva na tabela 
 function identifyFunctions()
-	for i = 1, #linhas do 
-  		if string.find(linhas[i], "function") ~= nil then			
-			functionsTable[#functionsTable + 1] = { ["name"] = getFunctionName(linhas[i]), ["pos"] = i}
+	for i = 1, #progLines do 
+  		if string.find(progLines[i], "function") ~= nil then			
+			functionsTable[#functionsTable + 1] = { ["name"] = getFunctionName(progLines[i]), ["pos"] = i+1}
 		end
 	end
 end
@@ -63,8 +64,107 @@ function printfunctionsTable()
 	end
 end
 
+--Procura onde a função se inicia 
+function searchFunctionPositionInFunctionsTable(functionName)
+	for i = 1, #functionsTable do 
+		if (functionsTable[i].name == functionName) then
+			return functionsTable[i].pos 
+		end
+	end
+end
+
+--Executa a função passada como parametro
+function executeFunction(functionName)
+	local stackPosition = #stackExecution + 1
+	local funcPosition = searchFunctionPositionInFunctionsTable(functionName)
+
+	-- if isThereParametersInThisFunction(funcPosition) then
+	-- end
+
+	if isThereVariablesInThisFunction(funcPosition+1) then
+		stackExecution[stackPosition].variables = getVariablesNameAndValues(funcPosition+1)
+	end
+end
+
+--Verifica se a função possui variáveis.
+function isThereVariablesInThisFunction(lineNumber)
+	--Se existir a palavra "begin" quer dizer que não há variáveis locais.
+	if isThereBEGINInThisLine(lineNumber) then
+		return true
+	else
+		return false
+	end
+end
+
+function isThere_BEGIN_InThisLine(lineNumber)
+	local str = string.match(progLines[lineNumber], "begin")
+	if (str == "begin") then 
+		return true
+	else
+		return false
+	end	
+end
+
+--Retorna uma lista com o nome das variaveis simples e dos vetores (nesse caso salva o tamanho do vetor)
+function getVariablesList(lineNumber)
+	local variablesAndValues = {}
+	local i = lineNumber
+	--Se não há "begin" quer dizer que a contém as declarações das variáveis
+	while (not isThere_BEGIN_InThisLine(i)) do
+
+		if (isVariableANumber(i)) then
+			local nameField = getVariableName(i)
+			print(nameField)
+		else
+			local nameField = getVariableName(i)
+			local vectorSize = getVectorSize(i)
+			print(nameField, vectorSize)
+		end
+
+		i = i + 1
+	end
+end
+
+--Pega o nome da variável seja ela uma variável simples ou um vetor
+function getVariableName(lineNumber)
+	--Pego tudo depois do espaço em branco após a palavra "var" até o primeiro caracter "[" (caso ele exista).
+	local varName = string.match(progLines[lineNumber], "var [^%[]*")
+	--print("linha completa: "..varName)
+	--Removo o "var " que restou no inicio da string 
+	varName = string.sub(varName, 5)
+	return varName
+end
+
+function getVariableValue(lineNumber)
+	local varValue = string.match(progLines[lineNumber], " %d+")
+	return varValue
+end
+
+--Pega o tamanho do vetor que foi declarado
+function getVectorSize(lineNumber)
+	local vectorSize = string.match(progLines[lineNumber], "%[(%d+)%]")
+	return vectorSize
+end
+
+--Verifica se a variável é um número ou vetor. Se for número o retorna true, se vetor, false.
+function isVariableANumber(lineNumber)
+	local str = string.find(progLines[lineNumber], "%[")
+	--Se for nil então não existe o caractere "[", então é é um número comum, caso contrário é um vetor
+	if str == nil then 
+		return true
+	else
+		return false
+	end
+end
+
+-- function isThereParametersInThisFunction(lineNumber)
+-- 	if
+-- 	return true
+-- end
+
+
 --Faz o pré processmento
---Salva o arquivo lido na tabela "linhas" e procura pelas funções existentes no programa e as salva em "functionsTable"
+--Salva o arquivo lido na tabela "progLines" e procura pelas funções existentes no programa e as salva em "functionsTable"
 function preProcessing()
 	prepareFile()
 	identifyFunctions()
@@ -76,8 +176,9 @@ end
 
 ------------------------Execução do Programa principal-------------------------------------------------------------------------
 preProcessing()
+getVariablesList(2)
 
-
+--executeFunction("main")
 
 
 -------------------------------------------------------------------------------------------------------------------------------
