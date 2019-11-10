0e6636079296ba1b83fbfa958473e892a4300a9f
Victor Alexandre <38512776+victor-alexandre@users.noreply.github.com>
2019-11-10 00:33:24 -0300

    Update interpretadorVictor.lua
    Adicionado forma de salvar na pilha as atribuições das variáveis, e foi adicionado a verificação do arrayOutBounds

--- interpretadorVictor.lua
+++ interpretadorVictor.lua
@@ -1,4 +1,5 @@
 -------------------------------Variaveis globais--------------------------------------------------------------------------------
+pretty = require('pl.pretty')
 progLines = {}
 functionsTable = {}
 stackExecution = {}
@@ -11,6 +12,7 @@ function initializeVectorWithZeros(vectorSize)
 	for i = 1, vectorSize do
 		myVector[i] = 0
 	end
+	pretty.dump(myVector)
 	return myVector
 end
 
@@ -29,11 +31,29 @@ function printVector(myVector)
 end
 
 --Verifica se o indice que está tentando ser acessado existe. Caso não exista retornar mensagem de erro.
-function verifyArrayOutBounds()
-
-	--print("Erro: o índice acessado está fora dos limites do array")
+function verifyArrayOutBounds(index, size)
+	if index > size then
+		print("Erro: índice fora dos limites do array")
+		os.exit(1)
+	end
 end
 
+--Transforma os indices negativos em positivos e coloca de uma forma que exista a posição 0
+--O INDICE PASSADO É 0 MAS ELE ACESSARÁ A POSIÇÃO 1 
+function resolveArrayIndex(index, size)
+	local newIndex
+	if index < 0 then
+		--Não confunda, estou fazendo uma soma, mas como index é negativo então na verdade isso vira uma subtração
+		--Somo 1 para acessar a posição 1 de lua e não 0
+		newIndex = index + size + 1
+	else
+		newIndex = index + 1
+	end
+	verifyArrayOutBounds(newIndex, size)
+	return newIndex
+end
+
+--Função minha para diferenciar os prints e o que eu queria realmente debugar
 function debugLine(...)
 	print(...)
 end
@@ -58,7 +78,7 @@ end
 
 
 --Pega o nome da variável seja ela uma variável simples ou um vetor
-function getVariableName(lineNumber)
+function getVariableNameIn_Declaration(lineNumber)
 	--Pego tudo após a palavra "var" + n characteres em branco até o primeiro caracter "[" (caso ele exista).
 	local str = string.match(progLines[lineNumber], "var%s+[^%[]*")
 
@@ -75,9 +95,14 @@ function getVariableValue(lineNumber)
 	return varValue
 end
 
+--Pega o nome do vetor que está sendo usado dentro do corpo da função
+function getVectorName(str)
+	return string.match(str, "[^%[]*")
+end
+
 --Pega o tamanho do vetor que foi declarado
-function getVectorSize(lineNumber)
-	local vectorSize = tonumber( string.match(progLines[lineNumber], "%[(%d+)%]") )
+function getVectorNumber(str)
+	local vectorSize = tonumber( string.match(str, "%[(%-*%d+)%]") )
 	return vectorSize
 end
 
@@ -108,7 +133,26 @@ function removeReservedWordAndAllWhiteSpaces(reservedWord, str)
 	local wihoutReservedWord = string.gsub(str, regexPattern, "") 
 	local wihoutReservedWordAndWhiteSpace = string.gsub(wihoutReservedWord, "%s*", "") 
 	return wihoutReservedWordAndWhiteSpace
+end
 
+--Verifica se a linha atual contém a palavra "begin"
+function isThere_BEGIN_InThisLine(lineNumber)
+	local str = string.match(progLines[lineNumber], "^begin")
+	if (str == "begin") then 
+		return true
+	else
+		return false
+	end	
+end
+
+--Verifica se a linha atual contém a palavra "end"
+function isThere_END_InThisLine(lineNumber)
+	local str = string.match(progLines[lineNumber], "^end")
+	if (str == "end") then 
+		return true
+	else
+		return false
+	end	
 end
 -------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------
@@ -117,7 +161,7 @@ end
 --Abre o arquivo e salva ele localmente na variavel global progLines
 function prepareFile()
 	-- Pega o nome do arquivo passado como parâmetro (se houver)
-	local filename = "./testes/teste8.bpl"
+	local filename = "./testes/teste5.bpl"
 	if not filename then
 	   print("Usage: lua interpretador.lua <prog.bpl>")
 	   os.exit(1)
@@ -134,7 +178,7 @@ function prepareFile()
 	file:close()
 end	
 
---Salva o arquivo na variavel global progLines. Observe que ele será salvo com os comentários removidos
+--Salva o arquivo na variavel global progLines. Observe que ele será salvo com os comentários removidos e sem espaços iniciais
 function saveFile(file)
 	for line in file do
 		local tempStr = removeComments(line)
@@ -175,19 +219,20 @@ function executeFunction(functionName)
 	stackExecution[stackPosition] = {}
 	initializeReturnValue(stackPosition)
 	declareVariables(funcPosition,stackPosition)
-	executeFunctionBody(bodyBegin, bodyEnd)
+	executeFunctionBody(bodyBegin, bodyEnd, stackPosition)
 
+	return stackExecution[stackPosition]["ret"]
 end
 
 -- Executa o conteúdo presente no corpo da função, observe que esse conteúdo se inicia 1 linha após o "begin" e finaliza 1 linha antes do "end"
-function executeFunctionBody(bodyBegin, bodyEnd)
+function executeFunctionBody(bodyBegin, bodyEnd, stackPosition)
 	for lineNumber = bodyBegin + 1, bodyEnd - 1 do 
 		if verifyAction(lineNumber) == "attribuition" then
-			resolveAttribuition(lineNumber)
+			resolveAttribuition(lineNumber, stackPosition)
 		elseif verifyAction(lineNumber) == "comparation" then
-			resolveComparation(lineNumber)
+			resolveComparation(lineNumber, stackPosition)
 		elseif verifyAction(lineNumber) == "functionCall" then
-			resolveFunctionCall(lineNumber)
+			resolveFunctionCall(lineNumber, stackPosition)
 		else
 			print("Erro na execução do corpo")
 		end
@@ -209,17 +254,103 @@ function verifyAction(lineNumber)
 end
 
 --Resolve atribuição
-function resolveAttribuition(lineNumber)
+function resolveAttribuition(lineNumber, stackPosition)
 	print(progLines[lineNumber], "atribuição")
+	local myLeftVarName, myLeftVarIndex = getLeftSide(lineNumber)
+	local myRightValue = getRightSideAndResolve(lineNumber)
+
+	--Se não há index então a variavel é simples, logo precisamos apenas colocar o valor dela
+	if myLeftVarIndex == nil then
+		stackExecution[stackPosition].simpleVariables[myLeftVarName] = myRightValue
+	else
+		--Antes de tentar atribuir o valor iremos verificar se é possível acessar o indice do vetor e fazer as alterações para podermos
+		--Receber índices iniciados com 0 pq na linguagem do BRUNO os vetores iniciam com o INDICE 0
+		myLeftVarIndex = resolveArrayIndex(myLeftVarIndex, stackExecution[stackPosition].vectorVariables[myLeftVarName].size)
+		print("INDICE: ", myLeftVarIndex, "VALOR: ", myRightValue)
+		stackExecution[stackPosition].vectorVariables[myLeftVarName].values[myLeftVarIndex] = myRightValue
+	end
+	
 end
 
+--Retorna o nome da variável que está presente no lado esquerdo da equação
+function getLeftSide(lineNumber)
+	local myStr = progLines[lineNumber]
+	--Lê tudo até o primeiro espaço em branco.
+	local leftSide = string.match(myStr, "^(%S+)")
+	--Se for vetor retorna o nome do vetor e o índice
+	if string.match(leftSide, "%[") == "[" then
+		return getVectorName(leftSide), tonumber(getVectorNumber(leftSide))
+	--Se não retorna o valor original que será o nome da variavel simples
+	else
+		return leftSide
+	end
+end
+
+--Resolve e retorna o valor do lado direito da equação como número ou o
+function getRightSideAndResolve(lineNumber)
+	local myStr = progLines[lineNumber]
+	local operation = verifyOperation(lineNumber)
+
+	if operation == "singleValue" then
+		local value = getRightSingleValue(lineNumber)
+		--Se for diferente de nil é porque é um número
+		if tonumber(value) ~= nil then
+        	return tonumber(value)
+        --Se não for número então é uma variável
+		else
+		end
+	else
+		return 99
+	end	
+end
+
+function getValueFromStack()
+end
+
+function putValueInStack()
+end
+
+--Retorna o único valor presente no lado direito seja ele um número ou uma variável
+function getRightSingleValue(lineNumber)
+	local myStr = progLines[lineNumber]
+	--Lê tudo até o primeiro espaço em branco
+	local rightValue = string.match(myStr, "=(.*)")
+	--Removo o sinal de atribuição
+	rightValue = string.gsub(rightValue, "=", "") 
+	--Removo os espaços em branco
+	rightValue = string.gsub(rightValue, "%s", "") 
+	print("lado direito da equação: ", rightValue)
+	return rightValue
+end
+
+
+function verifyOperation(lineNumber)
+	local myStr = progLines[lineNumber]
+
+	if string.match(myStr, " + ") == " + " then
+		return "+"
+	elseif string.match(myStr, " - ") == " - " then
+		return "-"
+	elseif string.match(myStr, " / ") == " / " then
+		return "/"
+	elseif string.match(myStr, " * ") == " * " then
+		return "*"		
+	elseif string.match(myStr, "%(") == "(" then
+		return "functionCall"
+	else
+		return "singleValue"
+	end			
+end
+
+
+
 --Resolve comparação (IF-ELSE)
-function resolveComparation(lineNumber)
+function resolveComparation(lineNumber, stackPosition)
 	print(progLines[lineNumber], "comparação")
 end
 
 --Resolve chamada de função
-function resolveFunctionCall(lineNumber)
+function resolveFunctionCall(lineNumber, stackPosition)
 	print(progLines[lineNumber], "chamada de função")
 end
 
@@ -250,25 +381,7 @@ function isThereVariablesInThisFunction(lineNumber)
 	end
 end
 
---Verifica se a linha atual contém a palavra "begin"
-function isThere_BEGIN_InThisLine(lineNumber)
-	local str = string.match(progLines[lineNumber], "^begin")
-	if (str == "begin") then 
-		return true
-	else
-		return false
-	end	
-end
 
---Verifica se a linha atual contém a palavra "end"
-function isThere_END_InThisLine(lineNumber)
-	local str = string.match(progLines[lineNumber], "^end")
-	if (str == "end") then 
-		return true
-	else
-		return false
-	end	
-end
 
 --Retorna uma lista com as variáveis(simples e vetores) declaradas e inicializadas com 0's
 function getVariablesList(lineNumber)
@@ -278,15 +391,15 @@ function getVariablesList(lineNumber)
 	while (not isThere_BEGIN_InThisLine(lineNumber)) do
 
 		if isVariableANumber(lineNumber) then
-			local nameField = getVariableName(lineNumber)
+			local nameField = getVariableNameIn_Declaration(lineNumber)
 			--Como na declaração da variável não há valor iremos iniciar com 0--Isso está sendo pedido no trabalho
 			local value = 0
 			--Os valores serão armazenados em um campo com o nome da variável
 			simpleVariables[nameField] = value
 
 		else
-			local nameField = getVariableName(lineNumber)
-			local vectorSize = getVectorSize(lineNumber)
+			local nameField = getVariableNameIn_Declaration(lineNumber)
+			local vectorSize = getVectorNumber(progLines[lineNumber])
 			--Como na declaração da variável não há valor iremos iniciar o vetor com 0's--Isso está sendo pedido no trabalho
 			local vectorValues = initializeVectorWithZeros(vectorSize)
 			vectorVariables[nameField] = {size = vectorSize, values = vectorValues}
@@ -349,10 +462,14 @@ executeFunction("main")
 
 print("stackExecutionSize: ", #stackExecution)
 
-local pretty = require('pl.pretty')
+
+
+
 pretty.dump(stackExecution)
-pretty.dump(functionsTable)
-pretty.dump(progLines)
+--pretty.dump(functionsTable)
+--pretty.dump(progLines)
+
+print("tentativa de acessar vetor: ", stackExecution[1].vectorVariables["a"].values[1])
 
 -------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------
