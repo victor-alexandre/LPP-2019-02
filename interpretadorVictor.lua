121c3e4889830037ab634228fb888a53eb03e623
Victor Alexandre <38512776+victor-alexandre@users.noreply.github.com>
2019-11-10 21:39:16 -0300

    Update interpretadorVictor.lua
    Adicionado tratamento do IF-ELSE

--- interpretadorVictor.lua
+++ interpretadorVictor.lua
@@ -3,6 +3,7 @@ pretty = require('pl.pretty')
 progLines = {}
 functionsTable = {}
 stackExecution = {}
+local filename = ...
 -------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------
 -------------------------------Declaração das funções Auxiliares---------------------------------------------------------------
@@ -12,7 +13,6 @@ function initializeVectorWithZeros(vectorSize)
 	for i = 1, vectorSize do
 		myVector[i] = 0
 	end
-	pretty.dump(myVector)
 	return myVector
 end
 
@@ -61,10 +61,18 @@ end
 -------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------
 -------------------------------Declaração das funções Regex--------------------------------------------------------------------
---Remove os comenários que estão após o //
+--Remove os comenários que estão após o //--FUNÇÃO NÃO ESTÁ FUNCIONANDO COMO O ESPERADO
 function removeComments(line)
-	--debugLine(string.match(line, "[^//]*"))
-	return string.match(line, "[^//]*")
+	if string.find(line, "//") ~= nil then
+		local lixo = string.match(line,"[^//]*$")
+		print("NOVA LINHA", string.gsub(line, lixo, ""))
+		local semLixo = string.gsub(line, lixo, "")
+		print(semLixo)
+		return semLixo	
+	else
+		print(line)
+		return line
+	end
 end
 
 function removeInitialWhiteSpaces(str)
@@ -91,26 +99,28 @@ end
 
 --Pega o valor da variável
 function getVariableValue(lineNumber)
-	local varValue = tonumber( string.match(progLines[lineNumber], " %d+") )
+	local varValue = tonumber(string.match(progLines[lineNumber], " %d+"))
 	return varValue
 end
 
 --Pega o nome do vetor que está sendo usado dentro do corpo da função
 function getVectorName(str)
-	return string.match(str, "[^%[]*")
+	local vectorName = string.match(str, "[^%[]*")
+	vectorName = string.gsub(vectorName, "%s*", "") 
+	return vectorName
 end
 
---Pega o tamanho do vetor que foi declarado
-function getVectorNumber(str)
-	local vectorSize = tonumber( string.match(str, "%[(%-*%d+)%]") )
-	return vectorSize
+--Pega o número do índice presente nos colchetes vetor
+function getVectorIndex(str)
+	local vectorIndex = tonumber(string.match(str, "%[(%-*%d+)%]"))
+	return vectorIndex
 end
 
 --Verifica se a variável é um número ou vetor. Se for número o retorna true, se vetor, false.
-function isVariableANumber(lineNumber)
-	local str = string.find(progLines[lineNumber], "%[")
+function isVariableANumber(str)
+	local myStr = string.find(str, "%[")
 	--Se for nil então não existe o caractere "[", então é é um número comum, caso contrário é um vetor
-	if str == nil then 
+	if myStr == nil then 
 		return true
 	else
 		return false
@@ -161,7 +171,7 @@ end
 --Abre o arquivo e salva ele localmente na variavel global progLines
 function prepareFile()
 	-- Pega o nome do arquivo passado como parâmetro (se houver)
-	local filename = "./testes/teste5.bpl"
+	
 	if not filename then
 	   print("Usage: lua interpretador.lua <prog.bpl>")
 	   os.exit(1)
@@ -181,8 +191,8 @@ end
 --Salva o arquivo na variavel global progLines. Observe que ele será salvo com os comentários removidos e sem espaços iniciais
 function saveFile(file)
 	for line in file do
-		local tempStr = removeComments(line)
-		tempStr = removeInitialWhiteSpaces(tempStr)
+		--local tempStr = removeComments(line)
+		local tempStr = removeInitialWhiteSpaces(line)
 		progLines[#progLines + 1] = tempStr
 		--debugLine(tempStr)
 	end
@@ -208,6 +218,11 @@ end
 
 --Executa a função passada como parametro
 function executeFunction(functionName)
+
+	if functionName == "print" then
+		print(args)
+	end
+
 	local stackPosition = #stackExecution + 1
 	local funcPosition, bodyBegin, bodyEnd = getFunctionDataInFunctionsTable(functionName)
 
@@ -226,15 +241,19 @@ end
 
 -- Executa o conteúdo presente no corpo da função, observe que esse conteúdo se inicia 1 linha após o "begin" e finaliza 1 linha antes do "end"
 function executeFunctionBody(bodyBegin, bodyEnd, stackPosition)
-	for lineNumber = bodyBegin + 1, bodyEnd - 1 do 
+	local lineNumber = bodyBegin + 1
+	while  lineNumber < bodyEnd do
 		if verifyAction(lineNumber) == "attribuition" then
 			resolveAttribuition(lineNumber, stackPosition)
+			lineNumber = lineNumber + 1
 		elseif verifyAction(lineNumber) == "comparation" then
-			resolveComparation(lineNumber, stackPosition)
+			--Resolve comparation vai retornar o numero de linhas que já foi executado dentro dele
+			lineNumber = lineNumber + resolveComparation(lineNumber, stackPosition)
 		elseif verifyAction(lineNumber) == "functionCall" then
 			resolveFunctionCall(lineNumber, stackPosition)
+			lineNumber = lineNumber + 1
 		else
-			print("Erro na execução do corpo")
+			--print("Erro na execução do corpo")
 		end
 	end
 end
@@ -256,30 +275,38 @@ end
 --Resolve atribuição
 function resolveAttribuition(lineNumber, stackPosition)
 	print(progLines[lineNumber], "atribuição")
-	local myLeftVarName, myLeftVarIndex = getLeftSide(lineNumber)
-	local myRightValue = getRightSideAndResolve(lineNumber)
+	local myLeftVarName, myLeftVarIndex = resolveLeftSide(lineNumber)
+	local myRightValue = resolveRightSide(lineNumber, stackPosition)
 
-	--Se não há index então a variavel é simples, logo precisamos apenas colocar o valor dela
+	--Se não há index na variável então a variavel é simples, logo precisamos apenas colocar o valor dela dentro da stack
 	if myLeftVarIndex == nil then
-		stackExecution[stackPosition].simpleVariables[myLeftVarName] = myRightValue
+		putVariableValueInStack(myLeftVarName, stackPosition, myRightValue)
 	else
 		--Antes de tentar atribuir o valor iremos verificar se é possível acessar o indice do vetor e fazer as alterações para podermos
 		--Receber índices iniciados com 0 pq na linguagem do BRUNO os vetores iniciam com o INDICE 0
 		myLeftVarIndex = resolveArrayIndex(myLeftVarIndex, stackExecution[stackPosition].vectorVariables[myLeftVarName].size)
-		print("INDICE: ", myLeftVarIndex, "VALOR: ", myRightValue)
-		stackExecution[stackPosition].vectorVariables[myLeftVarName].values[myLeftVarIndex] = myRightValue
+		putVectorValueInStack(myLeftVarName, myLeftVarIndex, stackPosition, myRightValue)
 	end
-	
+end
+
+--Coloca o valor de um vetor, em um indice especifico, dentro da stack
+function putVectorValueInStack(vectorName, vectorIndex, stackPosition, value)
+	stackExecution[stackPosition].vectorVariables[vectorName].values[vectorIndex] = value
+end
+
+--Coloca o valor de uma varíável dentro da stack
+function putVariableValueInStack(variableName, stackPosition, value)
+	stackExecution[stackPosition].simpleVariables[variableName] = value
 end
 
 --Retorna o nome da variável que está presente no lado esquerdo da equação
-function getLeftSide(lineNumber)
+function resolveLeftSide(lineNumber)
 	local myStr = progLines[lineNumber]
 	--Lê tudo até o primeiro espaço em branco.
 	local leftSide = string.match(myStr, "^(%S+)")
 	--Se for vetor retorna o nome do vetor e o índice
 	if string.match(leftSide, "%[") == "[" then
-		return getVectorName(leftSide), tonumber(getVectorNumber(leftSide))
+		return getVectorName(leftSide), getVectorIndex(leftSide)
 	--Se não retorna o valor original que será o nome da variavel simples
 	else
 		return leftSide
@@ -287,31 +314,120 @@ function getLeftSide(lineNumber)
 end
 
 --Resolve e retorna o valor do lado direito da equação como número ou o
-function getRightSideAndResolve(lineNumber)
+function resolveRightSide(lineNumber, stackPosition)
 	local myStr = progLines[lineNumber]
 	local operation = verifyOperation(lineNumber)
 
 	if operation == "singleValue" then
-		local value = getRightSingleValue(lineNumber)
-		--Se for diferente de nil é porque é um número
-		if tonumber(value) ~= nil then
-        	return tonumber(value)
-        --Se não for número então é uma variável
+		local rightValue = getRightSideSingleValue(lineNumber)
+		--Se for diferente de nil é porque é um número puro então retornamos ele
+		if tonumber(rightValue) ~= nil then
+        	return tonumber(rightValue)
+        --Se não for número então é uma variável, então devemos pegar o valor da variável antes de retornar
 		else
+			rightValue = getVariableValueFromStack(rightValue, stackPosition)
+			return rightValue
 		end
-	else
-		return 99
+	elseif operation == "functionCall" then
+		return resolveFunctionCall(myStr)
+	elseif operation == "+" or operation == "-" or operation == "/" or operation == "*" then
+		print("ENTROU AQUI PELO MENOS")
+		return resolveOperation(lineNumber, stackPosition)
 	end	
 end
 
-function getValueFromStack()
+--Resolve a operação que estará do lado direito da atribuição
+function resolveOperation(lineNumber, stackPosition)
+	local lixo, leftValue, lixo2, operation, lixo3, rightValue = string.match(progLines[lineNumber], "(=%s+)([^%s]*)(%s+)(.)(%s+)([^%s]*)")
+	--print("LeftValue", leftValue, "RightValue", rightValue)
+
+	--Primeiramente resolvemos o valor da esquerda e da direita Depois verificamos qual foi a operação solicitida e fazemos o calculo
+	local solvedRightValue, solvedLeftValue
+	--Se ele for um número então ele será usado diretamente se não o valor dele será buscado dentro da stack
+	if tonumber(rightValue) ~= nil then
+		solvedRightValue = rightValue
+	--Se ele for função então devemos resolver ela para pegar o valor
+	elseif isFunction(rightValue) then
+		solvedRightValue = resolveFunctionCall()
+	--Se não for nem função nem valor puro então ele é uma variável e o valor dele será buscado dentro da stack
+	else 
+		solvedRightValue = getVariableValueFromStack(rightValue, stackPosition)
+	end
+
+
+	if tonumber(leftValue) ~= nil then
+		solvedLeftValue = leftValue
+	elseif isFunction(leftValue) then
+		solvedLeftValue = resolveFunctionCall()
+	else 
+		solvedLeftValue = getVariableValueFromStack(leftValue, stackPosition)
+	end
+
+
+	if operation == "+" then
+		return math.floor(solvedLeftValue + solvedRightValue)
+	elseif operation == "-" then
+		return math.floor(solvedLeftValue - solvedRightValue)
+	elseif operation == "*" then
+		return math.floor(solvedLeftValue * solvedRightValue)
+	elseif operation == "/" then
+		return math.floor(solvedLeftValue / solvedRightValue)
+	end
 end
 
-function putValueInStack()
+--Verifica se a string passada é uma função
+function isFunction(str)
+	if string.match(str, "%(") == "(" then
+		return true
+	else
+		return false
+	end
 end
+
+-- function resolveSum(leftValue, rightValue, lineNumber, stackPosition)
+-- 	local solvedRightValue, solvedLeftValue
+-- 	--Se ele for um número então ele será usado diretamente se não o valor dele será buscado dentro da stack
+-- 	if tonumber(rightValue) ~= nil then
+-- 		solvedRightValue = rightValue
+-- 	--Se ele for função então devemos resolver ela para pegar o valor
+-- 	elseif isFunction(rightValue) then
+-- 		solvedRightValue = resolveFunctionCall()
+-- 	--Se não for nem função nem valor puro então ele é uma variável e o valor dele será buscado dentro da stack
+-- 	else 
+-- 		solvedRightValue = getVariableValueFromStack(rightValue, stackPosition)
+-- 	end
+
+-- 	if tonumber(leftValue) ~= nil then
+-- 		solvedLeftValue = leftValue
+-- 	elseif isFunction(leftValue) then
+-- 		solvedLeftValue = resolveFunctionCall()
+-- 	else 
+-- 		solvedLeftValue = getVariableValueFromStack(leftValue, stackPosition)
+-- 	end
+
+-- 	return math.floor(solvedLeftValue + solvedRightValue)
+-- end
+
+
+
+
+function getVariableValueFromStack(varName, stackPosition)
+	print("String Recebida "..varName)
+	--Verificamos se a variável é um numero ou vetor
+	if isVariableANumber(varName) then
+		return stackExecution[stackPosition].simpleVariables[varName]
+	else
+		local myVectorVar = getVectorName(varName)
+		local myVectorIndex = getVectorIndex(varName)
+		--Faço as transformações necessárias para gerar um indice válido
+		local newIndex = resolveArrayIndex(myVectorIndex, stackExecution[stackPosition].vectorVariables[myVectorVar].size)
+		return stackExecution[stackPosition].vectorVariables[myVectorVar].values[newIndex]
+	end
+end
+
 
 --Retorna o único valor presente no lado direito seja ele um número ou uma variável
-function getRightSingleValue(lineNumber)
+function getRightSideSingleValue(lineNumber)
 	local myStr = progLines[lineNumber]
 	--Lê tudo até o primeiro espaço em branco
 	local rightValue = string.match(myStr, "=(.*)")
@@ -319,7 +435,7 @@ function getRightSingleValue(lineNumber)
 	rightValue = string.gsub(rightValue, "=", "") 
 	--Removo os espaços em branco
 	rightValue = string.gsub(rightValue, "%s", "") 
-	print("lado direito da equação: ", rightValue)
+	--print("lado direito da equação: ", rightValue)
 	return rightValue
 end
 
@@ -327,13 +443,13 @@ end
 function verifyOperation(lineNumber)
 	local myStr = progLines[lineNumber]
 
-	if string.match(myStr, " + ") == " + " then
+	if string.match(myStr, " %+ ") == " + " then
 		return "+"
-	elseif string.match(myStr, " - ") == " - " then
+	elseif string.match(myStr, " %- ") == " - " then
 		return "-"
-	elseif string.match(myStr, " / ") == " / " then
+	elseif string.match(myStr, " %/ ") == " / " then
 		return "/"
-	elseif string.match(myStr, " * ") == " * " then
+	elseif string.match(myStr, " %* ") == " * " then
 		return "*"		
 	elseif string.match(myStr, "%(") == "(" then
 		return "functionCall"
@@ -346,12 +462,91 @@ end
 
 --Resolve comparação (IF-ELSE)
 function resolveComparation(lineNumber, stackPosition)
-	print(progLines[lineNumber], "comparação")
+	local ifWord, leftValue, lixo, comparationSimbol, lixo2, rightValue  = string.match(progLines[lineNumber], "(if%s+)([^%s]*)(%s+)([^%s]*)(%s+)([^%s]*)")
+	local solvedLeftValue, solvedRightValue
+	print(string.match(progLines[lineNumber], "(if%s+)([^%s]*)(%s+)([^%s]*)(%s+)([^%s]*)"))
+
+	--Resolvemos o lado esquerdo da comparação
+	if tonumber(leftValue) ~= nil then
+		solvedLeftValue = leftValue
+	else
+		solvedLeftValue = getVariableValueFromStack(leftValue, stackPosition)
+	end
+
+	--Resolvemos o lado direito da comparação
+	if tonumber(rightValue) ~= nil then
+		solvedRightValue = rightValue
+	else
+		solvedRightValue = getVariableValueFromStack(rightValue, stackPosition)
+	end
+
+
+	--Se for verdadeiro executa o comando dentro do corpo do if
+	if comparationSimbol == "==" and solvedLeftValue == solvedRightValue then
+		resolveAttribuition(lineNumber+1, stackPosition)
+	elseif comparationSimbol == "==" and (not solvedLeftValue == solvedRightValue) then 
+		--verifica se tem else, se sim executa o comando dentro do corpo do else
+		if existsElse(lineNumber+2) then
+			resolveAttribuition(lineNumber+3, stackPosition)
+		end
+	--------------------------------------------------------------------------
+	elseif comparationSimbol == "!=" and solvedLeftValue ~= solvedRightValue then
+		resolveAttribuition(lineNumber+1, stackPosition)
+	elseif comparationSimbol == "!=" and (not solvedLeftValue ~= solvedRightValue) then 
+		if existsElse(lineNumber+2) then
+			resolveAttribuition(lineNumber+3, stackPosition)
+		end			
+	--------------------------------------------------------------------------
+	elseif comparationSimbol == "<" and solvedLeftValue < solvedRightValue then
+		resolveAttribuition(lineNumber+1, stackPosition)
+	elseif comparationSimbol == "<" and (not solvedLeftValue < solvedRightValue) then 
+		if existsElse(lineNumber+2) then
+			resolveAttribuition(lineNumber+3, stackPosition)
+		end		
+	--------------------------------------------------------------------------
+	elseif comparationSimbol == "<=" and solvedLeftValue <= solvedRightValue then
+		resolveAttribuition(lineNumber+1, stackPosition)
+	elseif comparationSimbol == "<=" and (not solvedLeftValue <= solvedRightValue) then 
+		if existsElse(lineNumber+2) then
+			resolveAttribuition(lineNumber+3, stackPosition)
+		end	
+	---------------------------------------------------------------------------
+	elseif comparationSimbol == ">" and solvedLeftValue > solvedRightValue then
+		resolveAttribuition(lineNumber+1, stackPosition)
+	elseif comparationSimbol == ">" and (not solvedLeftValue > solvedRightValue) then 
+		if existsElse(lineNumber+2) then
+			resolveAttribuition(lineNumber+3, stackPosition)
+		end		
+	elseif comparationSimbol == ">=" and solvedLeftValue >= solvedRightValue then
+		resolveAttribuition(lineNumber+1, stackPosition)
+	elseif comparationSimbol == ">=" and (not solvedLeftValue >= solvedRightValue) then 
+		if existsElse(lineNumber+2) then
+			resolveAttribuition(lineNumber+3, stackPosition)
+		end	
+	end
+
+	--Vou retornar o Número de linhas do corpo dessa estrutura IF-ELSE
+	if existsElse(lineNumber+2) then
+		return 5
+	else
+		return 3
+	end
+end
+
+--Verifica se existe else na linha
+function existsElse(lineNumber)
+	if string.match(progLines[lineNumber], "else") == "else" then
+		return true
+	else
+		return false
+	end
 end
 
 --Resolve chamada de função
-function resolveFunctionCall(lineNumber, stackPosition)
-	print(progLines[lineNumber], "chamada de função")
+function resolveFunctionCall(str)
+	--Pega o nome da função
+	local funcName = string.match(str, "%s+[^%(]*")
+	return 0
 end
 
 --Inicializa o valor de retorno de uma função com 0
@@ -390,7 +585,7 @@ function getVariablesList(lineNumber)
 	--Se não há "begin" quer dizer que ainda estamos em uma linha que contém as declarações das variáveis
 	while (not isThere_BEGIN_InThisLine(lineNumber)) do
 
-		if isVariableANumber(lineNumber) then
+		if isVariableANumber(progLines[lineNumber]) then
 			local nameField = getVariableNameIn_Declaration(lineNumber)
 			--Como na declaração da variável não há valor iremos iniciar com 0--Isso está sendo pedido no trabalho
 			local value = 0
@@ -399,7 +594,7 @@ function getVariablesList(lineNumber)
 
 		else
 			local nameField = getVariableNameIn_Declaration(lineNumber)
-			local vectorSize = getVectorNumber(progLines[lineNumber])
+			local vectorSize = getVectorIndex(progLines[lineNumber])
 			--Como na declaração da variável não há valor iremos iniciar o vetor com 0's--Isso está sendo pedido no trabalho
 			local vectorValues = initializeVectorWithZeros(vectorSize)
 			vectorVariables[nameField] = {size = vectorSize, values = vectorValues}
@@ -448,7 +643,6 @@ function preProcessing()
 	prepareFile()
 	identifyFunctions()
 	identifyBodyFunction()
-	printfunctionsTable()--REMOVER DEPOIS ESSA LINHA**********************************************************************************
 end
 -------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------
@@ -463,13 +657,10 @@ executeFunction("main")
 print("stackExecutionSize: ", #stackExecution)
 
 
-
-
+pretty.dump(functionsTable)
 pretty.dump(stackExecution)
 --pretty.dump(functionsTable)
 --pretty.dump(progLines)
-
-print("tentativa de acessar vetor: ", stackExecution[1].vectorVariables["a"].values[1])
 
 -------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------
